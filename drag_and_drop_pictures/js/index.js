51afcf45763f5cf36e78ab44a11ffdbf8069293f
BenHeubl <techjournalism@gmail.com>
2016-05-07 07:44:47 +0100

    added the javascript drag and drop functions section

---
+++ drag_and_drop_pictures/js/index.js
@@ -0,0 +1,16 @@
+// // create an es6 class with class
+//
+// class dataoutput {
+//         constructor(description, wheels) {
+//         this.description = description;
+//         this.wheels = wheels;
+//         }
+//         myFunction() {
+//         console.log(${this.description})
+//         }
+// }
+//
+// var example1 = new dataoutput("Auto", 10);
+//
+// example1.myFunction();
+// // end
